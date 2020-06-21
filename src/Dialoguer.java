8e9b8c351db095f89c022b9f5d4de3e4caf71cec
jucod <jucod@ventilator.lan>
2020-06-21 17:04:38 +0200

    first

---
+++ src/Dialoguer.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Dialoguer {
+	public void parler(String question);
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dialoguer d = (s) -> System.out.println("Tu as dis : " + s);
+		d.parler("Bonjour");
+
+	}
+
+}
